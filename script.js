4c21ca30d0a624e1de71254ef127378e02caaf79
wendrias <39842984+wendrias@users.noreply.github.com>
2022-08-16 01:26:19 -0400

    front page and projects page updates

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function showPage() {
+    document.querySelector('.invisible').classList.add('visible');
+    document.querySelector('.invisible').classList.remove('invisible');
+
+}
+
+
 document.getElementById('menu-icon').onclick = () => {
     if (document.getElementById('nav').classList.contains('nav-show')) {
         document.getElementById('nav').classList.remove('nav-show');
@@ -7,9 +14,32 @@ document.getElementById('menu-icon').onclick = () => {
         document.getElementById('nav').classList.add('nav-show')
         document.getElementById('menu-icon').classList.add('nav-show');
     }
+};
+
+//background change
+let newGradient = () => {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+    let colorHex = "#";
+
+    //letter or num
+    for (let i = 0; i < 6; i++) {
+        let randNum = Math.floor(Math.random() * 2) + 1;
+        console.log(randNum);
+
+        if (randNum == 1) { //add random letter
+            colorHex = colorHex + letters[Math.floor(Math.random() * 5)]
+        } else { //add random number
+            let newrand = Math.floor(Math.random() * 9) + 1;
+            console.log(newrand);
+            colorHex = colorHex + newrand;
+        }
+        console.log(colorHex)
+    }
+    return colorHex;
 }
 
-function showPage() {
-    document.querySelector('.invisible').classList.add('visible');
-    document.querySelector('.invisible').classList.remove('invisible');
+if (document.getElementsByTagName('body')[0].id == "home-body") {
+    document.querySelectorAll('#home-body')[0].onclick = () => {
+        document.querySelectorAll('body')[0].setAttribute('style', 'background-image: linear-gradient\(' + newGradient() + ',' + newGradient() + '\)');
+    }
 }
