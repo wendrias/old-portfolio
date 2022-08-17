6b573de52d976a06b7a900955e79d834b92bf76b
wendrias <39842984+wendrias@users.noreply.github.com>
2022-08-17 15:11:22 -0400

    style, js, and project edits

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 function showPage() {
     document.querySelector('.invisible').classList.add('visible');
     document.querySelector('.invisible').classList.remove('invisible');
-
+    document.querySelectorAll('body')[0].setAttribute('style', 'background-image: linear-gradient\(' + sessionStorage.getItem('color1') + ',' + sessionStorage.getItem('color2') + '\)');
+    document.querySelectorAll('#projects-body')[0].setAttribute('style', 'background-image: linear-gradient\(' + sessionStorage.getItem('color1') + ',' + sessionStorage.getItem('color2') + ',' + sessionStorage.getItem('color1') + '\) !important');
+    document.querySelectorAll('nav')[0].setAttribute('style', "background: " + sessionStorage.getItem('color1') + ";");
 }
 
 //exit page transition  
-
 function leavePage(url) {
     document.querySelector('.visible').classList.add('invisible');
     document.querySelector('.visible').classList.remove('visible');
@@ -18,23 +19,25 @@ function leavePage(url) {
 document.getElementById('menu-icon').onclick = () => {
     if (document.getElementById('nav').classList.contains('nav-show')) {
         document.getElementById('nav').classList.remove('nav-show');
-
         document.getElementById('menu-icon').classList.remove('nav-show');
+        document.querySelectorAll('nav')[0].setAttribute('style', "background: " + sessionStorage.getItem('color2') + "!important;");
     } else {
         document.getElementById('nav').classList.add('nav-show')
         document.getElementById('menu-icon').classList.add('nav-show');
+        document.querySelectorAll('nav')[0].setAttribute('style', "background: " + sessionStorage.getItem('color1') + ";");
     }
 };
 
 //background change
-let newGradient = () => {
+let newGradients = () => {
     const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
-    let colorHex = "#";
+    gradients = [];
+    colorHex = "#";
 
-    //letter or num
+    //first color
     for (let i = 0; i < 6; i++) {
+        //letter or num
         let randNum = Math.floor(Math.random() * 2) + 1;
-        console.log(randNum);
 
         if (randNum == 1) { //add random letter
             colorHex = colorHex + letters[Math.floor(Math.random() * 5)]
@@ -43,13 +46,38 @@ let newGradient = () => {
             console.log(newrand);
             colorHex = colorHex + newrand;
         }
-        console.log(colorHex)
+        // console.log(colorHex);
     }
-    return colorHex;
+    sessionStorage.setItem('color1', colorHex)
+    colorHex = "#";
+
+
+    //second color
+    for (let i = 0; i < 6; i++) {
+        //letter or num
+        let randNum = Math.floor(Math.random() * 2) + 1;
+
+        if (randNum == 1) { //add random letter
+            colorHex = colorHex + letters[Math.floor(Math.random() * 5)]
+        } else { //add random number
+            let newrand = Math.floor(Math.random() * 9) + 1;
+            console.log(newrand);
+            colorHex = colorHex + newrand;
+        }
+        // console.log(colorHex)
+    }
+    gradients.push(colorHex);
+    sessionStorage.setItem('color2', colorHex)
+        // return colorHex;
 }
+
+// function setBackground() {
+
+// }
 
 if (document.getElementsByTagName('body')[0].id == "home-body") {
     document.querySelectorAll('#home-content')[0].onclick = () => {
-        document.querySelectorAll('body')[0].setAttribute('style', 'background-image: linear-gradient\(' + newGradient() + ',' + newGradient() + '\)');
+        newGradients();
+        document.querySelectorAll('body')[0].setAttribute('style', 'background-image: linear-gradient\(' + sessionStorage.getItem('color1') + ',' + sessionStorage.getItem('color2') + '\)');
     }
 }
