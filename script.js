2aa8293e8182bf4ddf5f77745200cf9f81035a1c
wendrias <39842984+wendrias@users.noreply.github.com>
2022-09-02 17:30:36 -0400

    add profile img and add project details

--- script.js
+++ script.js
@@ -68,13 +68,96 @@ let newGradients = () => {
         // return colorHex;
 }
 
-// function setBackground() {
 
-// }
-
+//background change
 if (document.getElementsByTagName('body')[0].id == "home-body") {
     document.querySelectorAll('#home-content')[0].onclick = () => {
         newGradients();
         document.querySelectorAll('body')[0].setAttribute('style', 'background-image: linear-gradient\(' + sessionStorage.getItem('color1') + ',' + sessionStorage.getItem('color2') + '\)');
+    }
+}
+
+//projects page details toggle
+function showDetails(el, carousel, link, img) {
+    console.log(el)
+    console.log(carousel)
+    console.log(link)
+    console.log(img)
+
+    let popup = document.querySelector('.project-details-wrapper');
+
+    //get elements from card
+    let title = el.querySelector('.desc h3').innerText;
+    let text = el.querySelector('.desc p').innerText;
+    let numTags = el.querySelector('.tag-wrapper').children.length;
+
+    //set elements
+    popup.querySelector('.desc h3').innerText = title;
+    popup.querySelector('.desc p').innerText = text;
+    popup.querySelector('img').setAttribute('src', img)
+    if (link != '') {
+        popup.querySelector('.visit-button').setAttribute('style', "display:block;");
+        popup.querySelector('.visit-button').setAttribute('href', link)
+    } else {
+        popup.querySelector('.visit-button').setAttribute('style', "display:none;");
+    }
+
+    //show popup
+    popup.setAttribute('style', 'display: block;')
+
+    //tags
+    popup.querySelector('.tag-wrapper').innerHTML = "";
+    let tagArr = Array.from(el.querySelector('.tag-wrapper').children);
+    tagArr.forEach((t) => {
+        popup.querySelector('.tag-wrapper').innerHTML = popup.querySelector('.tag-wrapper').innerHTML + t.outerHTML;
+    })
+
+
+    // set if carousel needed
+    if (carousel) {
+        popup.querySelectorAll('.carousel span').forEach(e => e.setAttribute('style', 'display:block;'))
+    } else {
+        popup.querySelectorAll('.carousel span').forEach(e => e.setAttribute('style', 'display:none;'))
+    }
+
+}
+
+//close project details popup
+function closePopup() {
+    document.querySelector('.project-details-wrapper').setAttribute('style', 'display: none;')
+}
+
+//total images in carousel (for portfolio)
+const portfolioTotal = 6;
+
+function nextImg(projName) {
+    let total;
+    //get image number from img source
+    let num = Number(document.querySelector('.project-details-wrapper .proj-img').getAttribute('src').split('/').slice(-1)[0].split('.')[0]);
+    if (projName == 'portfolio') {
+        total = portfolioTotal;
+    } //add other cases for other projects
+
+    //setting new image
+    if (num < total) {
+        document.querySelector('.project-details-wrapper .proj-img').setAttribute('src', 'img/portfolio/' + (num + 1) + '.png');
+    } else {
+        document.querySelector('.project-details-wrapper .proj-img').setAttribute('src', 'img/portfolio/1.png');
+    }
+}
+
+function prevImg(projName) {
+    let total;
+    //get image number from img source
+    let num = Number(document.querySelector('.project-details-wrapper .proj-img').getAttribute('src').split('/').slice(-1)[0].split('.')[0]);
+    if (projName == 'portfolio') {
+        total = portfolioTotal;
+    } //add other cases for other projects
+
+    //setting new image
+    if (num > 1) {
+        document.querySelector('.project-details-wrapper .proj-img').setAttribute('src', 'img/portfolio/' + (num - 1) + '.png');
+    } else {
+        document.querySelector('.project-details-wrapper .proj-img').setAttribute('src', '/img/portfolio/' + total + '.png');
     }
 }
